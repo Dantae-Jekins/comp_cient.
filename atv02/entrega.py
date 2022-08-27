8e4c78eacd35c0cd2d6bfcd9d513f88e4f1a1e0d
Dantae-Jekins <dantae.sona.jekins@gmail.com>
2022-08-27 00:17:58 -0300

    Entrega 02 pronta, sem certeza se está certo

---
+++ atv02/entrega.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+if __name__ == "__main__":
+    file_altu = None;
+    file_anos = None;
+
+    ## <tratamento de erro  (ignora)>
+    try: file_altu = open("altura.txt", "r");
+    except IOError as e:
+        print("Erro open()", e );
+        file_altu = None;
+    
+    try: file_anos = open("anos.txt", "r");
+    except IOError as e:
+        print("Erro open()", e);
+        file_anos = None;   
+    ## </tratamento de erro>
+
+    if ( file_altu == None or file_anos == None):
+        print("Encerrando programa");
+
+    else:
+        ## next para menor consumo de memória
+        line_altu = next(file_altu);
+        line_anos = next(file_anos);
+        altu_array = np.empty(0, dtype=np.float64);
+        
+        while 1:
+            ## condições de break
+            try: line_altu = next(file_altu);
+            except: break;
+            try: line_anos = next(file_anos);
+            except: break;
+           
+            ## filtração dos dados 
+            ## eu não vou me dar o trabalho de tratar erros em caso de tipo inválido
+            ## pelo fato do tratamento de erro no python ser HORRÍVEL
+            ano = float(line_anos);
+            ## não foi definido se o intervalo é inclusivo ou não, pela semântica assume-se que não
+            if (ano > 1.998e+3 and ano < 2.005e+3):
+                altu_array = np.append(altu_array, float(line_altu));
+
+        ## conclusão dos dados
+        soma = np.sum(altu_array);
+        media = soma / np.size(altu_array);
+        print("Média das alturas dos atletas nascidos entre 1998 e 2005.");
+        print(media);
